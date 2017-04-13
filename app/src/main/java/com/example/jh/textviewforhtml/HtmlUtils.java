0dc29269361efc58efcfea2faf1796bfe7db2685
renxh3 <renxh3@lenovo.com>
2017-04-13 15:23:15 +0800

    Merge branch 'master' of https://github.com/renxh4/TextViewForHtml2
    # Conflicts:
    #	app/src/main/java/com/example/jh/textviewforhtml/HtmlUtils.java

--- app/src/main/java/com/example/jh/textviewforhtml/HtmlUtils.java
+++ app/src/main/java/com/example/jh/textviewforhtml/HtmlUtils.java
@@ -1,10 +1,12 @@
 package com.example.jh.textviewforhtml;
 
+import android.util.Log;
+
 /**
  * Created by jh on 2017/2/13.
  */
 public class HtmlUtils {
     public HtmlUtils(){
-        
+
     }
 }
