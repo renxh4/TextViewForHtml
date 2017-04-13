7cf29d5df9fab8d5f5254a9f6a84b67594462080
chailei <chaieli9005@163.com>
2017-04-13 13:55:29 +0800

    add log

--- app/src/main/java/com/example/jh/textviewforhtml/HtmlUtils.java
+++ app/src/main/java/com/example/jh/textviewforhtml/HtmlUtils.java
@@ -1,10 +1,13 @@
 package com.example.jh.textviewforhtml;
 
+import android.util.Log;
+
 /**
  * Created by jh on 2017/2/13.
  */
 public class HtmlUtils {
     public HtmlUtils(){
         //hello,我是master
+        Log.d("HtmlUtils","test merge");
     }
 }
